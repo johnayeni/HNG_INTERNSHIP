3f8212e5e591153c953c300f649cff790c8e7dc0
johnayeni <john.ayeni@stu.cu.edu.ng>
2018-04-10 11:54:47 +0100

    chore: remove bootstrap library

--- stage1/app/index.php
+++ stage1/app/index.php
@@ -1,40 +1,55 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>HNG4 | Stage 1</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
   <style>
     body{
       font-family: 'Raleway', sans-serif;
     }
+    .card {
+      position: relative;
+      color: grey;
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-orient: vertical;
+      -webkit-box-direction: normal;
+      -ms-flex-direction: column;
+      flex-direction: column;
+      min-width: 0;
+      word-wrap: break-word;
+      background-color: #fff;
+      background-clip: border-box;
+      border: 1px solid rgba(0,0,0,.125);
+      border-radius: .25rem;
+      width: 20rem;
+    }
+    .card > img {
+      margin: auto;
+    }
+    .card > h1 {
+      text-align: center;
+    }
+    .card > h2 {
+      text-align: center;
+    }
   </style>
 </head>
 <body style="background-color: #2D9CDB;">
-  <div class="container-fluid">
-    <div class="row justify-content-center" style="margin-top: 100px;">
+    <div style="margin-top: 100px;">
       <div class="col-md-4">
-          <div class="card" style="border: none;box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);">
-            <div class="text-center">
-              <img class="img-fluid rounded-circle" src="http://placehold.it/180x180" alt="Card image cap" width="200" style="padding: 20px;">
-              <div class="card-body" style="color: grey;">
-                <h2>WELCOME</h2>
-                <h1>
-                <?php
-                    echo $timestamp = date('H:i:s');
-                ?> 
-                </h1>
-              </div>    
-            </div>
+          <div class="card" style="border: none;box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23); margin: auto;">
+            <img src="http://placehold.it/180x180" alt="Card image cap" width="200" style="border-radius: 100%; padding: 20px;">
+            <h2>WELCOME</h2>
+            <?php
+              echo $timestamp = date('H:i:s');
+            ?> 
           </div>
       </div>
     </div>
-  </div>
 </body>
 </html>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
